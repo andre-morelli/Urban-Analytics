d7da23b17f77e199fdc56c9b0da37cf7c9e59a56
unknown <andre.morelli@usp.br>
2022-07-28 13:02:52 -0300

    New funcs

--- tools/curve_funcs.py
+++ tools/curve_funcs.py
@@ -18,8 +18,13 @@ def e_cumulative_linear(parameters, *data):
 def e_cumulative_gauss(parameters, *data):
     x,y=data
     t,v=parameters
+    return (((x<=t)*1 + (x>t)*np.exp(-(x-t)/v)-y)**2).mean()
+
+def e_soft_threshold(parameters, *data):
+    x,y=data
+    t,k=parameters
     if t >= 0:
-        return (((x<=t)*1 + (x>t)*np.exp(-(x-t)/v)-y)**2).mean()
+        return ((((1+np.exp(k*(x-t)/t))**-1)-y)**2).mean()
     else:
         return np.inf
 
@@ -28,12 +33,12 @@ def e_mod_gauss(parameters, *data):
     b=parameters
     return ((np.exp(-x**2/b)-y)**2).mean()
     
-def e_soft_threshold(parameters, *data):
+def e_mod_log_logit(parameters, *data):
     x,y=data
-    c,t=parameters
+    k,c=parameters
     
     if t > 0:
-        return ((1/(1+np.exp(c*(x-t)/t)) * (1+np.exp(-c)) - y)**2).mean()
+        return ((1/(1+np.exp(k+c*np.log(x))) - y)**2).mean()
     else:
         return np.inf
     
@@ -61,22 +66,51 @@ def cumulative_gauss(x,t,v):
 def mod_gauss(x,b):
     return np.exp(-x**2/b)
 
-def soft_threshold(x,c,t):
-    if t > 0:
-        return 1/(1+np.exp(c*(x-t)/t)) * (1+np.exp(-c))
-    else:
-        return -np.inf
+def soft_threshold(x, t=500, k=5):
+    """
+    Calculate soft threshold accessibility for array of observations.
+    source: Higgs, C., Badland, H., Simons, K. et al. The Urban Liveability 
+    Index: developing a policy-relevant urban liveability composite measure 
+    and evaluating associations with transport mode choice. Int J Health 
+    Geogr 18, 14 (2019). https://doi.org/10.1186/s12942-019-0178-8
+    
+    Parameters
+    ----------
+    d : numpy array
+        Array where every entry is a travel cost.
+    t : float
+        function parameter (point at which access score = 0.5).
+    k : float
+        function parameter
+    
+    Returns
+    -------
+    Numpy array
+    """
+    return (1+np.exp(k*(x-t)/t))**-1
+
+def mod_log_logit(x,k,c):
+    return 1/(1+np.exp(k+c*np.log(x+(x==0)*1e-8)))
 
 def inv_pow(x,b):
     x = x + (x==0)*1e-8
     return (x<=1)*1 + (x>1)*(x**-b)
 
-ALL_ACCESSIBILITY_FUNCS = {
+ALL_ACCESSIBILITY_FUNCS_PDF = {
         'Inverse Exponential' : (e_exp, exp, {'bounds': [(0,5)]}, ['beta']),
         'Cumulative Rectangular' : (e_cumulative,cumulative, {'bounds': [(0,1200)]},['t']),
         'Cumulative Linear' : (e_cumulative_linear,cumulative_linear, {'bounds': [(0,1200)]},['t']),
         'Cumulative Gaussian' : (e_cumulative_gauss,cumulative_gauss, {'bounds': [(0,200),(1,200)]},['t','v']),
         'Modified Gaussian' : (e_mod_gauss,mod_gauss,{'bounds':[(1e-3,5e4)]},['beta']),
-        'Soft Threshold' : (e_soft_threshold,soft_threshold,{'bounds':[(0,10),(1,1200)]},['c','t']),
-        'Inverse Power' : (e_inv_pow,inv_pow,{'bounds':[(1e-3,10)]},['beta'])
+        'Soft Threshold' : (e_soft_threshold,soft_threshold,{'bounds':[(0,10),(1,1200)]},['k','t'])
+    }
+    
+ALL_ACCESSIBILITY_FUNCS_CDF = {
+        'Inverse Exponential' : (e_exp, exp, {'p0': [1]}, ['beta']),
+        #'Cumulative Gaussian' : (e_cumulative_gauss,cumulative_gauss, {'p0':[20,1]},['t','v']),
+        'Soft Threshold' : (e_soft_threshold,soft_threshold,{'p0':[5,5]},['t','k']),
+        #'Cumulative Linear' : (e_cumulative_linear,cumulative_linear, {'p0': [500]},['t']),
+        'Modified Gaussian' : (e_mod_gauss,mod_gauss,{'p0':[1]},['beta']),
+        'Modified log-logit' : (e_mod_log_logit,mod_log_logit,{'p0':[1,1]},['k','c'])#,
+        #'Inverse Power' : (e_inv_pow,inv_pow,{'p0':1},['beta'])
     }
